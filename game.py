2c4e905293aa622c1f2f2ba1e8f99ad759fcd992
Petya Choeva <petya.choeva@gmail.com>
2024-04-24 22:54:59 +0100

    Added basic functions for random number, pokemon request. Added readme info.

--- game.py
+++ game.py
@@ -1,1 +1,18 @@
-print("Test")
+import random
+import requests
+from pprint import pprint
+
+pokemon_url = 'https://pokeapi.co/api/v2/pokemon/{}/'
+
+def getRandomNumber():
+    return random.randint(1,150)
+
+def getPokemon(pokemon_id):
+    request = pokemon_url.format(pokemon_id)
+    response = requests.get(request)
+    print(response.json())
+
+def game():
+    getPokemon(getRandomNumber())
+
+game()
